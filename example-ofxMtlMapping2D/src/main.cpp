cefda394b22f561ebad935e4ebed85e0240ba20c
smallfly <hugues@smallfly.com>
2013-05-08 15:54:30 -0400

    smoothing everything

--- example-ofxMtlMapping2D/src/main.cpp
+++ example-ofxMtlMapping2D/src/main.cpp
@@ -6,7 +6,8 @@
 int main( ){
 
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1280, 720, OF_WINDOW);			// <-------- setup the GL context
+    window.setGlutDisplayString("rgba double samples>=4");
+	ofSetupOpenGL(&window, 1024, 500, OF_WINDOW);			// <-------- setup the GL context
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
